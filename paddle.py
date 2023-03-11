7d390f5e0ae2284aa3cf1380ed499fa07735cb05
DamTran <thdam3012@gmail.com>
2023-03-11 20:24:44 +0200

    commit

--- paddle.py
+++ paddle.py
@@ -1,24 +1,24 @@
 from turtle import Turtle
 
 
-class Paddle:
+class Paddle(Turtle):
 
     def __init__(self, position):
-        self.paddle = Turtle(shape='square')
-        self.paddle.penup()
-        self.paddle.speed('fastest')
-        self.paddle.color('white')
-        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
-        self.paddle.goto(x=position[0], y=position[1])
+        super().__init__()
+        self.shape('square')
+        self.penup()
+        self.color('white')
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(x=position[0], y=position[1])
         self.new_y = 0
 
     def go_up(self):
-        self.new_y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), self.new_y)
+        self.new_y = self.ycor() + 20
+        self.goto(self.xcor(), self.new_y)
 
     def go_down(self):
-        self.new_y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), self.new_y)
+        self.new_y = self.ycor() - 20
+        self.goto(self.xcor(), self.new_y)
 
 
 
