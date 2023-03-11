6b45f567a19d03dc114aa556cf1517572a47efee
DamTran <thdam3012@gmail.com>
2023-03-11 20:16:18 +0200

    commit

--- main.py
+++ main.py
@@ -9,20 +9,19 @@ screen.title('Pong')
 screen.tracer(0)
 
 
-paddle = Paddle()
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
 
 screen.listen()
-screen.onkey(paddle.paddle_1_go_up, "Up")
-screen.onkey(paddle.paddle_1_go_down, "Down")
-screen.onkey(paddle.paddle_2_go_up, "w")
-screen.onkey(paddle.paddle_2_go_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 
 game_is_on = True
 while game_is_on:
     screen.update()
-
-
 
 
 
--- paddle.py
+++ paddle.py
@@ -1,37 +1,25 @@
 from turtle import Turtle
 
-POSITIONS = [[350, 0], [-350, 0]]
-PADDLES = []
-
 
 class Paddle:
 
-    def __init__(self):
-        for i in range(2):
-            self.paddle = Turtle(shape='square')
-            self.paddle.penup()
-            self.paddle.speed('fastest')
-            self.paddle.color('white')
-            self.paddle.shapesize(stretch_wid=5, stretch_len=1)
-            self.paddle.goto(x=POSITIONS[i][0], y=POSITIONS[i][1])
-            PADDLES.append(self.paddle)
-            self.new_y = 0
+    def __init__(self, position):
+        self.paddle = Turtle(shape='square')
+        self.paddle.penup()
+        self.paddle.speed('fastest')
+        self.paddle.color('white')
+        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
+        self.paddle.goto(x=position[0], y=position[1])
+        self.new_y = 0
 
-    def paddle_1_go_up(self):
-        self.new_y = PADDLES[0].ycor() + 20
-        PADDLES[0].goto(PADDLES[0].xcor(), self.new_y)
+    def go_up(self):
+        self.new_y = self.paddle.ycor() + 20
+        self.paddle.goto(self.paddle.xcor(), self.new_y)
 
-    def paddle_1_go_down(self):
-        self.new_y = PADDLES[0].ycor() - 20
-        PADDLES[0].goto(PADDLES[0].xcor(), self.new_y)
+    def go_down(self):
+        self.new_y = self.paddle.ycor() - 20
+        self.paddle.goto(self.paddle.xcor(), self.new_y)
 
-    def paddle_2_go_up(self):
-        self.new_y = PADDLES[1].ycor() + 20
-        PADDLES[1].goto(PADDLES[1].xcor(), self.new_y)
-
-    def paddle_2_go_down(self):
-        self.new_y = PADDLES[1].ycor() - 20
-        PADDLES[1].goto(PADDLES[1].xcor(), self.new_y)
 
 
 
